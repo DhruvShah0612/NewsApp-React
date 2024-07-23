984e8850168301d8da85533437be232dff83c7fa
Gitlab tutorial <dhruvshah0612@gmail.com>
2024-07-23 20:18:49 +0530

    chore: Add react-infinite-scroll-component and react-top-loading-bar dependencies

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
-      <div>
-        
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            className="card-img-top"
+            src={
+              !imageUrl
+                ? "https://bl-i.thgim.com/public/incoming/41ps91/article68391871.ece/alternates/LANDSCAPE_1200/IMG_PO26_stocks_2_1_R7CRHD2T.jpg"
+                : imageUrl
+            }
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "unknown" : author} on
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
